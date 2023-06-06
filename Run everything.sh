74a4de3a3d550db4196affd70c81a5ef288761b0
Alessandro Russo <alessandro.russo@datlas.it>
2023-06-06 09:30:48 +0200

    First implementation

--- Run everything.sh
+++ Run everything.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd app-shell && npm i && npm run start &
 
-cd .. && cd navbar && npm i && npm run serve:single-spa:navbar &
+cd navbar && npm i && npm run serve:single-spa:navbar &
 
-cd .. && cd section && npm i && npm run serve:single-spa:section &
+cd section && npm i && npm run serve:single-spa:section &
 
-cd .. && cd form && npm i && npm run serve:single-spa:form &&
+cd form && npm i && npm run serve:single-spa:form &&
 
 open http://localhost:9000/
